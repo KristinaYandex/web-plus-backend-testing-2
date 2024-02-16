9a2453ad0e910fc2ce8b57a469083e6329ee3acc
Kristina Ushakova <mramornayaa@yandex.ru>
2024-02-16 15:31:39 +0300

    Решение1

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,4 +1,4 @@
-import { PostsService } from './posts.service';
+import { FindManyOptions, PostsService } from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
@@ -20,11 +20,34 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const allPosts = postsService.findMany();
+      const expectedPosts = allPosts.map(item => {
+        return { text: item.text }
+      })
+      expect(expectedPosts).toEqual(posts)
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const allPosts = postsService.findMany({ limit: 2, skip: 1 });
+      const expectedPosts = allPosts.map(item => {
+        return { text: item.text }
+      })
+
+      expect(expectedPosts).toEqual([posts[1], posts[2]])
+    });
+
+    it('should return correct posts for skip option', () => {
+      const options: FindManyOptions = { skip: 8 };
+      const expectedPosts = posts.slice(8);
+      const foundPosts = postsService.findMany(options);
+      expect(foundPosts).toEqual(expectedPosts);
+    });
+
+    it('should return correct posts for limit option', () => {
+      const options: FindManyOptions = { limit: 8 };
+      const expectedPosts = posts.slice(0, 8);
+      const foundPosts = postsService.findMany(options);
+      expect(foundPosts).toEqual(expectedPosts);
     });
 
     // реализуйте недостающие тест-кейсы
